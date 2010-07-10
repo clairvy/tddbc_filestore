274a73f216e6812550524e2713fae39094618a56
NAGAYA Shinichiro <clairvy@gmail.com>
2010-07-10 16:29:57 +0900

    #仕様変更1 完了

--- src/main/scala/FileStore.scala
+++ src/main/scala/FileStore.scala
@@ -2,19 +2,27 @@
 package org.tddbcnagoya.filestore
 
 class FileStore {
-  var store:List[(String, String)] = List();
+  var valueStore:List[(String, String)] = List();
   def dump = {
-    store.foldLeft("")((i,p) => i + p._1 + ":" + p._2 + "\n")
+    valueStore.foldLeft("")((i,p) => i + p._1 + ":" + p._2 + "\n")
   }
   def set(k : String, v : String) {
     if (k == "" || k == null)
       return 
 
-    store = store.dropWhile( p => p._1 == k)
-    store = store ++ List((k, v))
+    valueStore = valueStore.dropWhile( p => p._1 == k)
+    valueStore = valueStore ++ List((k, v))
   }
 
   def get(k:String) = {
-    store.find(p => p._1 == k) map(_._2)
+    valueStore.find(p => p._1 == k) map(_._2)
+  }
+
+  def set_multi(l : List[(String, String)]) {
+    l.map(p => this.set(p._1, p._2))
+  }
+
+  def get_multi(l : List[String]) = {
+    valueStore.filter(p => l.exists(k => p._1 == k))
   }
 }
